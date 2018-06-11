39a6a359dce701038d515101d94fe55c7ffe5ceb
Luca Uburti <luca.uburti@gmail.com>
2018-06-12 00:40:08 +0200

    Created layout for Main RecyclerView items

--- app/src/main/java/uburti/luca/fitnessfordiabetics/MainActivity.java
+++ app/src/main/java/uburti/luca/fitnessfordiabetics/MainActivity.java
@@ -2,12 +2,21 @@ package uburti.luca.fitnessfordiabetics;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.main_rv)
+    RecyclerView mainRv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
     }
 }
